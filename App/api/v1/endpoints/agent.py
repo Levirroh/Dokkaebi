6c3343198430c943bf270637828c6ec5a12ede5f
Levirroh <johann.gr14@gmail.com>
2026-02-13 10:50:45 -0300

    update: new empty functions

--- App/api/v1/endpoints/agent.py
+++ App/api/v1/endpoints/agent.py
@@ -22,47 +22,18 @@ class Check(BaseModel):
 
 app = FastAPI()
 
-@app.post("/heartbeat/")
-async def heartbeat(req: Annotated[Check | None, Body()], header: Annotated[str | None, Header()] = None, session = Depends(get_db)):
-  try:
-    agent = jwt.decode(header, key, algorithms=["HS256"])
-    
-    db_agent = session.query(Agent).filter(Agent.id == agent.get("id")).first()
-    
-    if(db_agent != None):
-        db_agent.batery = req.batery
-        db_agent.status = "ONLINE"
-        db_agent.ultima_conexao = datetime.now()
-        
-        session.commit()
-    else:
-      raise HTTPException(status_code=404, detail="ERRO: Agente não encontrado.")
-    
-  except jwt.exceptions.InvalidTokenError:
-    raise HTTPException(status_code=401, detail="ERRO: Token Inválido ou Expirado.")
-
-  return {"message": "Dokkaebi online", "agent": agent}
-
-
-
-@app.get("/dashboard/")
-async def dashboard(session = Depends(get_db)):
-
-  result = session.query(Agent).all()
+@app.post("/send_heartbeat/")
+async def heartsend_heartbeatbeat(req: Annotated[Check | None, Body()], header: Annotated[str | None, Header()] = None, session = Depends(get_db)):
   
-  for agent in result:
-    status = try_connection(agent, session)
+  return True
 
-    if(status == "OFFLINE"):
-      agent.status ="OFFLINE"
-
-  return result
-
-async def try_connection(agent, session):
-  # Aqui você usaria uma biblioteca como 'httpx' ou 'subprocess' para dar um ping
-  # Estude a biblioteca 'httpx' para fazer requisições assíncronas
+@app.post("/status/")
+async def status(req: Annotated[Check | None, Body()], header: Annotated[str | None, Header()] = None, session = Depends(get_db)):
   
-  result = "ONLINE" #placeholder
+  return True
+
+@app.post("/photo/")
+async def photo(req: Annotated[Check | None, Body()], header: Annotated[str | None, Header()] = None, session = Depends(get_db)):
   
-    
-  return result
+  return True
+
