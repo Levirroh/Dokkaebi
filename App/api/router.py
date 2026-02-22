78cb1331147e46f3349e08f79c022fb7def43bdd
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-02-21 21:10:39 -0300

    fix: router name

--- App/api/router.py
+++ App/api/router.py
@@ -8,6 +8,6 @@ api_router = APIRouter()
 # Aqui você registra os módulos
 api_router.include_router(system_ep.router, prefix="/system", tags=["System"])
 api_router.include_router(auth_ep.router, prefix="/auth", tags=["Auth"])
-api_router.include_router(brain_ep.router, prefix="/master", tags=["Brain"])
+api_router.include_router(brain_ep.router, prefix="/brain", tags=["Brain"])
 api_router.include_router(master_ep.router, prefix="/master", tags=["Master"])
 api_router.include_router(agent_ep.router, prefix="/agents", tags=["Agents"])
