6089da0966a56c188b2855ee652aa34a3b9a7361
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-03-07 16:20:47 -0300

    feat: homepage inital commit

---
+++ Dokkaebi/src/pages/login/index.tsx
@@ -0,0 +1,37 @@
+import { HeaderTitle } from '@react-navigation/elements';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+export type Props = {
+  name: string;
+};
+
+function Hello({name} : Props) {
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.greeting}>
+        <HeaderTitle>Dokkaebi</HeaderTitle>
+        <Button
+          title="Entrar"
+          color="orange"
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 16,
+  },
+});
+
+export default Hello;
