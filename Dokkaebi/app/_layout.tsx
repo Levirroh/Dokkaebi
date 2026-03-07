3dd35fe31c37f71994808d28c5840f70877f40d2
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-03-07 16:20:15 -0300

    feat: adjusting layout page

--- Dokkaebi/app/_layout.tsx
+++ Dokkaebi/app/_layout.tsx
@@ -1,22 +1,19 @@
+//documento geral e padrão do app, tudo é padronizado por este, ex. fontes, background, etc.
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { HeaderShownContext } from '@react-navigation/elements';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{ headerShown: false }}> // headerShown: false - faz nao aparecer o caminho no header
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
