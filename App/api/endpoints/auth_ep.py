f10475bc18fcfb6d8d2722bb73da9bd1b8315417
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-03-08 18:06:07 -0300

    feat: login initial function

--- App/api/endpoints/auth_ep.py
+++ App/api/endpoints/auth_ep.py
@@ -1,8 +1,10 @@
 from typing import Annotated
-from fastapi import APIRouter, Body, Depends, FastAPI, Header
+from fastapi import APIRouter, Body, Depends, FastAPI, HTTPException, Header
+import jwt
 from pydantic import BaseModel
 
 from App.database.session import get_db
+from App.models.user_model import User
 
 key = "#ifoieibnvienv"
 
@@ -13,6 +15,29 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+class Login(BaseModel):
+    token_dokka: str # dokka id already inside dokka token
+
+
 
 app = FastAPI()
+
+@app.post("/login/")
+async def logs(req: Annotated[Login | None, Body()], header: Annotated[str | None, Header()] = None, session = Depends(get_db)):
+  try:
+    login = jwt.decode(header, key, algorithms=["HS256"])
+    
+    password = login["password"]
+    username = login["username"]
+    
+    user = session.select(User).where(User.username == login["username"] and User.usernamepassword == jwt.encode(login["password"], key, algorithm=["HS256"])).first()
+    
+    if(not user):
+        raise HTTPException(status_code=404, detail="ERRO: Usuário não encontrado.")
+        
+    
+  except jwt.exceptions.InvalidTokenError:
+    raise HTTPException(status_code=401, detail="ERRO: Token Inválido ou Expirado.")
+
+  return {"result": True}
 
