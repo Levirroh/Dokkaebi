0e1f7e1a241ecc81842785a946bb7a90252a3716
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-03-14 17:37:24 -0300

    minor changes

---
+++ Dokkaebi/src/pages/actions/index.tsx
@@ -0,0 +1,33 @@
+import { HeaderTitle } from '@react-navigation/elements';
+import { Button, StyleSheet, View } from 'react-native';
+
+export type Props = {
+  name: string;
+};
+
+function DashBoard({ name }: Props) {
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.menu}>
+        <HeaderTitle>Comandos</HeaderTitle>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  menu: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 16,
+  },
+});
+
+export default DashBoard;
