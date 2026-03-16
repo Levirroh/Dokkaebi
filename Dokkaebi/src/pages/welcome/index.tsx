1ad93d64725595557305b4151b7d657353e0c902
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-03-16 20:47:25 -0300

    feat: added inputs and message for warning/errors

--- Dokkaebi/src/pages/welcome/index.tsx
+++ Dokkaebi/src/pages/welcome/index.tsx
@@ -1,7 +1,7 @@
-import LoginModal from '@/src/components/loginModal';
+import Message from '@/src/components/message';
 import { HeaderTitle } from '@react-navigation/elements';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export type Props = {
   name: string;
@@ -10,22 +10,24 @@ export type Props = {
 function Welcome({ name }: Props) {
   const [isOpen, setIsOpen] = useState(false);
 
-  function changeModal(){
-  setIsOpen(!isOpen)
-}
+  function changeModal() {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.greeting}>
         <HeaderTitle>Dokkaebi</HeaderTitle>
+        <TextInput placeholder='Login' style={styles.input} />
+        <TextInput placeholder='Password' secureTextEntry={true} style={styles.input} />
         <Button
           title="Entrar"
           color="orange"
           onPress={changeModal}
         />
-        <LoginModal isOpen={isOpen} onClose={changeModal}></LoginModal>
+        <Message isShown={isOpen} onClose={changeModal} duration={3} message={"Usuário ou senha inválidos"}></Message>
       </View>
-    </View>
+    </View> 
   );
 }
 
@@ -39,6 +41,29 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     margin: 16,
+  },
+  overlay: {
+    flex: 1, 
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+  title: {
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    alignContent: 'center',
+  },
+  input: {
+    backgroundColor: 'gray',
+    color: 'black',
+  },
+  modalCard: {
+    width: '30%', 
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    elevation: 5,
   },
 });
 
