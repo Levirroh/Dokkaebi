2d52c7a3fc6baa7c982e13516829ea65d46bb2a5
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-04-04 19:19:00 -0300

    fix: router

--- App/api/router.py
+++ App/api/router.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 
-from App.api.endpoints import agent_ep, auth_ep, brain_ep, master_ep, system_ep
+from api.endpoints import agent_ep, auth_ep, brain_ep, master_ep, system_ep
 
 
 api_router = APIRouter()
 
 # Aqui você registra os módulos
-api_router.include_router(system_ep.router, prefix="/system", tags=["System"])
-api_router.include_router(auth_ep.router, prefix="/auth", tags=["Auth"])
-api_router.include_router(brain_ep.router, prefix="/brain", tags=["Brain"])
-api_router.include_router(master_ep.router, prefix="/master", tags=["Master"])
-api_router.include_router(agent_ep.router, prefix="/agents", tags=["Agents"])
+api_router.include_router(system_ep.router, tags=["System"])
+api_router.include_router(auth_ep.router, tags=["Auth"])
+api_router.include_router(brain_ep.router, tags=["Brain"])
+api_router.include_router(master_ep.router, tags=["Master"])
+api_router.include_router(agent_ep.router, tags=["Agents"])
