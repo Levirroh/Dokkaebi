f32256818a91d99be97420cee7160b0da16342b8
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-04-04 19:18:54 -0300

    feat: added function to easier projetct startup

--- App/main.py
+++ App/main.py
@@ -2,9 +2,9 @@ import select
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from App.api.router import api_router
-from App.database.session import get_db
-from App.models.user_model import User
+from api.router import api_router
+from database.session import get_db
+from models.user_model import User
 
 # Importe aqui o seu get_db e seus Models (ajuste o caminho se necessário)
 # from database import get_db
@@ -32,3 +32,8 @@ def test_db(db: Session = Depends(get_db)):
     statement = select(User).limit(1)
     result = db.execute(statement).scalars().first()
     return {"message": "Conexão com banco pronta para implementar"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
